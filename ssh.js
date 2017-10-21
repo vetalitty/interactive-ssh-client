6f1ba718a2166f4870ab8e7c9e9d79dc4e5d53d0
Vitaly Kuznetsov <vetal1950@gmail.com>
2017-10-21 20:40:39 +0800

    Last stable.

--- ssh.js
+++ ssh.js
@@ -15,7 +15,7 @@ class SSHtool {
             this.connection.on('ready', () => {
                 resolve();
             });
-            this.connection.on('error', (err) => {
+            this.connection.on('error', () => {
                 reject('error connect');
             });
         });
@@ -35,7 +35,7 @@ class SSHtool {
                     if (this.keyCodeStream._writableState.sync === false) {
                         process.stdout.write('' + data);
                     }
-                }).stderr.on('data', data => {
+                }).stderr.on('data', () => {
                     process.exit();
                 });
             });
@@ -51,7 +51,7 @@ class SSHtool {
                     }
                     let sshMessages = '';
                     stream
-                        .on('close', (code, signal) => {
+                        .on('close', () => {
                             resolve(sshMessages);
                         })
                         .on('data', data => {
@@ -65,26 +65,7 @@ class SSHtool {
         this._displayStartText();
         this._overrideButtons();
         await this.connect();
-        // todo: selectMode - default shell, get, pull
         this.shell();
-    }
-
-    _selectMode() {
-        if (process.argv.length <= 2) {
-            return this.shell();
-        }
-        if (process.argv.length >= 3 && process.argv.length <= 4) {
-            const method = process.argv[2];
-            const params = process.argv[3];
-            switch (method) {
-                case 'get':
-                    break;
-                case 'put':
-                    break;
-                default:
-                    throw new Error('Unsupported command.');
-            }
-        }
     }
 
     _displayStartText() {
