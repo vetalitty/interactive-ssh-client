9923a7cb3ef5e091bab89bff4c21421a4937b850
Vitaly Kuznetsov <vetal1950@gmail.com>
2017-10-21 22:10:05 +0800

    Try add get and put, but no successfully.

--- ssh.js
+++ ssh.js
@@ -1,5 +1,7 @@
 const Client = require('ssh2').Client;
 const config = require('./config.json');
+const fs = require('fs');
+let buf = '';
 
 class SSHtool {
     constructor(config) {
@@ -41,6 +43,24 @@ class SSHtool {
             });
     }
 
+    shellPutCommand(command) {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            self.connection
+                .shell((err, stream) => {
+                    if (err) reject(err);
+                    stream.write(command);
+                    stream.on('close', (data) => {
+                    }).on('data', data => {
+                        resolve(data);
+                    }).stderr.on('data', (err) => {
+                        reject(err);
+                        process.exit();
+                    });
+                });
+        });
+    }
+
     execCommand(commandStr) {
         let self = this;
         return new Promise((resolve, reject) => {
@@ -61,6 +81,36 @@ class SSHtool {
         });
     }
 
+    getFile(from, localPath) {
+        return new Promise((resolve, reject) => {
+            this.connection.sftp((err, sftp) => {
+                if (err) return reject(err);
+
+                sftp.fastGet(from, localPath, {}, err => {
+                    if (err) return reject(err);
+                    resolve('Successfully downloaded');
+                });
+            });
+        });
+    }
+
+    putFile(localPath, remotePath) {
+        return new Promise((resolve, reject) => {
+            this.connection.sftp((err, sftp) => {
+                if (err) return reject(err);
+
+                let readStream = fs.createReadStream(localPath);
+                let writeStream = sftp.createWriteStream(remotePath);
+
+                writeStream.on('end', () => {
+                    return resolve('Successfully uploaded');
+                });
+
+                readStream.pipe(writeStream);
+            });
+        });
+    }
+
     async init() {
         this._displayStartText();
         this._overrideButtons();
@@ -75,17 +125,38 @@ class SSHtool {
 
     _overrideButtons() {
         const ctrl_Q = '\u0011';
+        const ctrl_C = '\u0003';
+        const enter = '\u000d';
+        const tab = '\u0009';
         process.stdin.setRawMode(true);
         process.stdin.resume();
         process.stdin.setEncoding('utf8');
         process.stdin
             .on('data', (key) => {
+                process.stdout.write(key);
+                buf += key;
+                if (this.keyCodeStream && key === enter) {
+                    if (buf.search(/^(get\s)+/i) !== -1) {
+                        const filename = buf.substring(4, buf.length);
+                        this.shellPutCommand('echo $(pwd)\n').then(res => console.log('pwd:', res.toString())).catch(err => console.log(err));
+                        // ???
+                        buf = '';
+                    } else {
+                        this.keyCodeStream.write(buf + '\n');
+                        buf = '';
+                    }
+                }
                 if (key === ctrl_Q) process.exit();
-                if (this.keyCodeStream) this.keyCodeStream.write('' + key);
+                if (key === ctrl_C && this.keyCodeStream) this.keyCodeStream.write('' + key);
+                if (key === tab && this.keyCodeStream) this.keyCodeStream.write('' + key);
             });
     }
 }
 
 const ssh = new SSHtool(config);
 
-ssh.init().then().catch();
+ssh.init().then(res => {
+
+}).catch(err => {
+    console.log(err);
+});
